f24fe1580a3b3ff5ec5e2df09049e81061289956
Mustafoyev <mmustafoyev9@gmail.com>
2023-06-15 19:09:58 +0500

    commit message

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,5 +1,7 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/username/reposotoryName";
+    public static String REPO_LINK = "https://github.com/mmustafoyev/repository";
 }
+//https://github.com/mmustafoyev/repository.git
+//https://github.com/mmustafoyev/repository.git
